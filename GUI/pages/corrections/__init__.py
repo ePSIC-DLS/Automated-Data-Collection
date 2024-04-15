d2bc7457a68435fc6051cc723cd3adaafb659496
Maaaax2021 <78539265+Maaaax2021@users.noreply.github.com>
2024-04-15 10:05:59 +0100

    Delete old GUI

--- GUI/pages/corrections/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Subpackage to represent corrections that can be performed on the microscope.
-"""
-from .base import BaseCorrection
-from .beam_blank import Beam
-from .beam_flash import Emission
-from .autofocus import Focus
