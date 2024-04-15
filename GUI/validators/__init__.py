a847f54f66a749569c9554a4abe0d1acb7355de1
Maaaax2021 <78539265+Maaaax2021@users.noreply.github.com>
2024-04-15 10:04:35 +0100

    Delete old validators

--- GUI/validators/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._validators import *
-from ._validators import _Error as ValidationError
-from ._translators import *
-from ._translators import _Error as TranslationError
-from ._stages import *
-from ._instances import Examples as xmpls
