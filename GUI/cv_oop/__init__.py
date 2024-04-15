17f3175cc3339728f57fea32802334e54ff5f48d
Maaaax2021 <78539265+Maaaax2021@users.noreply.github.com>
2024-04-15 10:05:42 +0100

    Delete old images

--- GUI/cv_oop/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Package for working with cv2 images, but encapsulating the various methods into an OOP style.
-
-Creates classes for Colours and Images, as well as functions for various transforms.
-"""
-from ._enums import *
-from ._colours import Colour
-from ._images import Image
-from . import transforms
