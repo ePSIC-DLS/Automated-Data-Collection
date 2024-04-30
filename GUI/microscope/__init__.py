6aa875c5a49990b28112601b023bcac38611a92b
Maaaax2021 <78539265+Maaaax2021@users.noreply.github.com>
2024-04-30 12:04:04 +0100

    Reformat GUI directory

--- GUI/microscope/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import controllers
-from ._main import Controller as Microscope
-from ._utils import ONLINE, AptKind, ImagingMode, Detector, Lens, Axis, Driver, FullScan, AreaScan
-from .merlin_connection import MERLIN_connection as Merlin
-from ._engine import Scanner
