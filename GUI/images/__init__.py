af5d000614b847428d1856595ed6730ac060ed68
Maaaax2021 <78539265+Maaaax2021@users.noreply.github.com>
2024-04-30 12:03:43 +0100

    Reformat GUI directory

--- GUI/images/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._colours import GreyscaleColour as Grey, FullDepthColour as RGB, Colour
-from ._img import (RGBImage, GreyImage, RGBBimodal, GreyBimodal, ThresholdBehaviour, ThresholdType, BaseImage)
-from ._enums import *
-from ._errors import *
-from ._aliases import *
